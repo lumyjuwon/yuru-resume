532b5e068cc3071cea0d6a86852cec0c3013a595
Seong JuWon <32597561+lumyjuwon@users.noreply.github.com>
2021-07-14 21:59:47 +0900

    Update

--- resume/src/components/experience/experience.tsx
+++ resume/src/components/experience/experience.tsx
@@ -17,7 +17,7 @@ const companyExperience: CompanyExperience = {
       {
         name: '사내 Slack Server',
         description: `유저와 Slack간의 상호작용을 처리하는 서버입니다.
-        Slack에서 제공해주는 API 등을 이용해 젠킨스 빌드, 에러 로그 열람 등의 외부망에서 쉽게 접근하기 어려운 것들을 Slack을 통해 처리할 수 있도록 도구를 제공하고 있습니다.`,
+        Slack API를 이용해 젠킨스 빌드, 에러 로그 열람 등의 외부망에서 접근하기 어려운 것들을 Slack을 통해 처리할 수 있게 만들어 Slack만으로도 에러 확인 및 재빌드 등을 가능하게 만들어 외부망에서도 개발과 관련된 업무 등을 진행할 수 있도록 하였습니다.`,
         whatIdo: ['Slack을 통해 Jenkins Build 등을 처리하는 기능 개발', 'CI/CD 결과에 대한 세부적인 알림 처리'],
         whatIdoTitle: '담당 업무',
         period: '2021.05 ~ 현재',
@@ -25,8 +25,9 @@ const companyExperience: CompanyExperience = {
       },
       {
         name: 'Game Automation',
-        description: `Android, iOS, Windows 게임 QA자동화 프레임워크입니다.
-        해당 도구를 이용해 반복적인 컨텐츠 검증, 수동으로 진행해야 하는 프로파일링 등의 업무를 자동화 하여 효율적인 업무 프로세스를 구축했습니다.`,
+        description: `Android, iOS, Windows 게임 자동화 프레임워크입니다.
+        게임 자동화 프레임워크를 개발하게 된 이유는 컨텐츠 검증이나 프로파일링 등의 반복적인 업무들이 대부분 수동으로 이루어지고 있다는 것을 보고 자동화를 하게 된다면 엄청난 시너지를 보여줄 수 있을 것 같아 개발하게 됐습니다.
+        개발이 완료된 이후에는 사내에 반복적이었던 모든 업무들이 자동화 프레임워크를 통해 이루어지고 있고 이는 업무 생산성 증진에 큰 영향을 주었습니다.`,
         whatIdo: [
           'Jest 기반의 게임 자동화 Framework 설계 및 개발',
           'Appium을 이용한 Android, iOS, Windows 플랫폼 제어 개발',

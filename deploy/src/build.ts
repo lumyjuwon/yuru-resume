5aef467fcc00fc23e77dd1f7a84a1f289f6a31d8
Seong JuWon <32597561+lumyjuwon@users.noreply.github.com>
2021-10-24 20:54:00 +0900

    Implement build

--- deploy/src/build.ts
+++ deploy/src/build.ts
@@ -1,3 +1,8 @@
+import childProcess from 'child_process';
+import { PathMap } from './pathMap';
+
 export module Build {
-  export async function run() {}
+  export async function run() {
+    childProcess.execSync(`cd ${PathMap.resumePath} & yarn build`);
+  }
 }

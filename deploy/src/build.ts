85630c5281671df73d32a317b6d80bd8390f5b6c
Seong JuWon <32597561+lumyjuwon@users.noreply.github.com>
2021-10-24 21:21:46 +0900

    & -> &&

--- deploy/src/build.ts
+++ deploy/src/build.ts
@@ -4,7 +4,7 @@ import { PathMap } from './pathMap';
 export module Build {
   function buildReact(): Promise<void> {
     return new Promise((resolve, reject) => {
-      childProcess.exec(`cd ${PathMap.resumePath} & yarn run build`, (error, stdout, stderr) => {
+      childProcess.exec(`cd ${PathMap.resumePath} && yarn run build`, (error, stdout, stderr) => {
         if (error) {
           reject(error);
         }

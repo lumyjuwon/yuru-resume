ef605eb9fcfd306edc42d2bc109881fa4d145d74
Seong JuWon <32597561+lumyjuwon@users.noreply.github.com>
2021-10-31 00:52:09 +0900

    Update deploy.ts

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -1,20 +1,30 @@
-import { Afterbuild } from './src/afterbuild';
+import { Build } from './src/build/build';
+import { Afterbuild } from './src/build/afterbuild';
+import { Prebuild } from './src/build/prebuild';
 import { Args } from './src/args';
-import { Build } from './src/build';
-import { validatePathMap } from './src/pathMap';
-import { Prebuild } from './src/prebuild';
+import { User } from './src/config';
+import { PathMap, validatePathMap } from './src/pathMap';
+import { Converter } from './src/thirdParties/converter';
 
 (async () => {
-  const args = Args.parseArgs(process.argv);
+  await validatePathMap();
+  await User.load();
 
+  const args = Args.parseArgs(process.argv);
   if (args['-prebuild']) {
     await Prebuild.run();
     return;
   }
 
-  await validatePathMap();
-
   await Prebuild.run();
   await Build.run();
-  await Afterbuild.run();
+  await Afterbuild.run({
+    pagePath: User.config.setting.build.pagePath
+  });
+
+  await Converter.pdf({
+    htmlPath: `${PathMap.root}${User.config.setting.pdf.htmlPath}`,
+    outputPath: `${PathMap.root}${User.config.setting.pdf.outputPath}`,
+    format: User.config.setting.pdf.format
+  });
 })();

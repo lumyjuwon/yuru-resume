f9fe55f9bf26d1e435c07a29847468823fd74944
Seong JuWon <32597561+lumyjuwon@users.noreply.github.com>
2021-10-31 02:00:36 +0900

    Create header component

---
+++ client/src/components/common/header.tsx
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+import { Resources } from '../../resources';
+
+function DownloadHeader() {
+  return (
+    <a
+      style={{ marginLeft: '8px' }}
+      href={'https://github.com/lumyjuwon/lumy-resume/raw/master/sample/files/pdf.pdf'}
+      target='_blank'
+      rel='noreferrer'
+    >
+      <img src={Resources.iconResources.icons.pdf} width={36} alt={'Icon'} />
+    </a>
+  );
+}
+
+const Container = styled.div({});
+
+export function Header() {
+  return (
+    <Container>
+      <DownloadHeader />
+    </Container>
+  );
+}

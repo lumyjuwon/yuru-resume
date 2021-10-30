3f50c7138b5671b4f349faf25ee0261543777efb
Seong JuWon <32597561+lumyjuwon@users.noreply.github.com>
2021-10-31 00:51:56 +0900

    Refactory import order

--- deploy/src/pathMap.ts
+++ deploy/src/pathMap.ts
@@ -1,10 +1,10 @@
-import { promises as fs } from "fs";
-import path from "path";
+import path from 'path';
+import { promises as fs } from 'fs';
 
 export const PathMap = {
-  root: path.resolve(__dirname, "../../"),
-  userPath: path.resolve(__dirname, "../../user"),
-  clientPath: path.resolve(__dirname, "../../client"),
+  root: path.resolve(__dirname, '../../'),
+  userPath: path.resolve(__dirname, '../../user'),
+  clientPath: path.resolve(__dirname, '../../client')
 };
 
 export async function validatePathMap() {

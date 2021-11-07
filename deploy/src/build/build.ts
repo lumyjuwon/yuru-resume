604a24cbb825f98875049db587aa7ca063f07534
Seong JuWon <32597561+lumyjuwon@users.noreply.github.com>
2021-11-08 03:12:03 +0900

    Install client dependency

--- deploy/src/build/build.ts
+++ deploy/src/build/build.ts
@@ -1,12 +1,10 @@
 import { PathMap } from '../pathMap';
-import { Args } from '../utils/args';
+
 import { Utils } from '../utils/utils';
 
 export module Build {
   async function buildReact(): Promise<void> {
-    if (!Args.args.boolean['-is_github_action']) {
-      await Utils.exec(`cd ${PathMap.clientPath} && yarn`);
-    }
+    await Utils.exec(`cd ${PathMap.clientPath} && yarn`);
     await Utils.exec(`cd ${PathMap.clientPath} && yarn run build`);
   }
 

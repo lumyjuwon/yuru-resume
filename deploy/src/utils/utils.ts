d9d8460e1853a6a9e3d8adb7193cf4d7e397ee2e
Seong JuWon <32597561+lumyjuwon@users.noreply.github.com>
2021-11-12 01:20:20 +0900

    Add printing exec command log & handle not to reject about stderr

--- deploy/src/utils/utils.ts
+++ deploy/src/utils/utils.ts
@@ -3,13 +3,14 @@ import childProcess from 'child_process';
 export module Utils {
   export function exec(command: string): Promise<string> {
     return new Promise((resolve, reject) => {
+      console.log(`Running Command: ${command}`);
       childProcess.exec(command, (error, stdout, stderr) => {
         if (error) {
           reject(error);
         }
 
         if (stderr) {
-          reject(stderr);
+          console.error(stderr);
         }
 
         resolve(stdout);
